aee7ba10486145de5c47f168bd4e79e191614bcf
yusukeyasuo <yasuo.yusuke@gmail.com>
2012-10-17 00:30:15 +0900

    first commit

---
+++ Qiita/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Qiita
+//
+//  Created by yusuke_yasuo on 2012/10/13.
+//  Copyright (c) 2012年 yusuke_yasuo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UITableViewController
+
+@end
---
+++ Qiita/WebViewController.m
@@ -0,0 +1,38 @@
+//
+//  WebViewController.m
+//  Qiita
+//
+//  Created by yusuke_yasuo on 2012/10/13.
+//  Copyright (c) 2012年 yusuke_yasuo. All rights reserved.
+//
+
+#import "WebViewController.h"
+
+@interface WebViewController ()
+
+@end
+
+@implementation WebViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
